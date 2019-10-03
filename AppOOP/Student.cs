213912c317cb7c3e6223d082a1a8f03facc95c3d
Tien Anh <tienanh41161@mail.com>
2019-10-03 16:10:29 +0700

    Add project files.

---
+++ AppOOP/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppOOP
+{
+    class Student
+    {
+        public Student() { }
+        public Student(string stName)
+        {
+            Name = stName;
+        }
+        protected string Name;
+        public string StudentName
+        {
+            get
+            { return Name; }
+            set
+            { Name = value; }
+        }
+        public virtual void Talk()
+        {
+            if (StudentName.Equals("Minh"))
+                MessageBox.Show(" My name is " + this.StudentName + " i am a College Student");
+            else if (StudentName.Equals("Dung"))
+                MessageBox.Show(" My name is " + this.StudentName + " i am a University Student");
+        }
+    }
+}
